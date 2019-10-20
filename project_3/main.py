a87e742675f9049590b5d7505d56b326605d78b9
kingspp <kingspprathyush@gmail.com>
2019-10-19 22:10:03 -0400

    update params

--- project_3/main.py
+++ project_3/main.py
@@ -30,11 +30,11 @@ def parse():
     parser.add_argument('--use_pri_buffer', type=bool, default=True)
     parser.add_argument('--use_double_dqn', type=bool, default=True)
     parser.add_argument('--use_dueling', type=bool, default=False)
-    parser.add_argument('--lr', type=int, default=1e-2)
+    parser.add_argument('--lr', type=int, default=1e-3)
     parser.add_argument("--lr_scheduler", type=bool, default=True)
     parser.add_argument("--lr_min", type=float, default=1e-5)
     parser.add_argument("--lr_decay", type=float, default=0.9999)
-    parser.add_argument("--lr_decay_step", type=float, default=4)
+    parser.add_argument("--lr_decay_step", type=float, default=5)
 
 
     try:
